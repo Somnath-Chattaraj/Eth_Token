636f20f4d432fa26330614fef25bc9062606ab85
Somnath Chattaraj <somnathchattaraj5@gmail.com>
2024-12-22 17:13:46 +0530

    first commit

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,17 @@
-// SPDX-License-Identifier: Unlicense
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Contract { }
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    address public owner;
+
+    constructor(uint _initialValue) ERC20("MyToken", "MTK") {
+        _mint(msg.sender, _initialValue);
+        owner = msg.sender;
+    }
+    function mint(address to, uint256 amount) public {
+        require(msg.sender == owner, "only owner can mint");
+        _mint(to, amount);
+    }
+}
